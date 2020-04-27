55e71ac70ca89c2232fbf19c790aa7526be9cdc5
asshah4 <mrshahman@gmail.com>
2020-04-27 08:53:49 -0400

    cleaned up description wiht mainatiner, fixing up the read_hrv_matlab function

--- R/matlab_cardiovascular_signal_toolbox.R
+++ R/matlab_cardiovascular_signal_toolbox.R
@@ -7,7 +7,7 @@
 #' @param loc Location of the folder that contains all of the patients that were analyzed by the Main_HRV_Analysis.m function from the Toolbox.
 #' @param name Name of the patient/ID. There should exist a folder with the name inside the `loc` folder. Inside this folder are all the Toolbox parameters and HRV results in CSV format.
 #' @param vars Vector of the HRV variables to be collected
-#' @param time Number of seconds to group the HRV data by
+#' @param time Number of seconds to group the HRV data by. Defaults to 3600 seconds (which is 1 hour)
 #' @return Data frame of HRV summarized by the grouping variable (e.g. 3600 seconds = 1 hour). Also returns an additional column of percent missing (e.g. 20.0% missing data) by time group.
 #' @examples
 #' matlab_process_hrv(location, patid, hrv_variables, 3600)
@@ -21,36 +21,41 @@
 #' # Create for loop to eventually make data frame
 #' d <- list()
 #' for(i in 1:length(names)) {
-	#' d[[paste(names[i])]] <- matlab_process_hrv(loc, names[i], vars, time)
+	#' d[[paste(names[i])]] <- read_hrv_matlab(loc, names[i], vars, time)
 #' }
 #' df <- rbindlist(d) %>% as_tibble()
 #' @export
-read_matlab_hrv <- function(loc, name, vars, time) {
+read_hrv_matlab <- function(loc, name, time = 3600) {
 
-	# Selected HRV vars
-	svar <- c("t_start", "t_end", vars)
+	# Selected variables from HRV analysis
+	svar <- c("NNmean", "SDNN", "RMSSD", "pnn50", "ulf", "vlf", "lf", "hf", "lfhf", "ttlpwr", "ac", "dc", "SampEn", "ApEn")
+	vars <- c("patID", "t_start", "t_end", svar)
 
 	# Read in the HRV data
 	dt <-
 		list.files(file.path(loc, name), pattern = "HRV", full.name = TRUE) %>%
 		fread()
 
+	# Time grouping variable
+	### Need to add different increments
+	tvar <- ifelse(time/3600 == 1, "hour")
+
 	# Create a summary data set by grouping times
 	x <-
-		dt[, svar, with = FALSE
-		   ][, paste0("t_",time) := floor(t_start/time)
-		     ][, lapply(.SD, mean, na.rm=T), by=eval(paste0("t_", time)), .SDcols=vars]
+		dt[, vars, with = FALSE
+		   ][, paste0("t_",tvar) := floor(t_start/time)
+		     ][, lapply(.SD, mean, na.rm=T), by=eval(paste0("t_", tvar)), .SDcols=svar]
 
 	# Identify missing percent by group (for quality)
 	y <-
-		dt[, svar, with = FALSE
-		   ][, paste0("t_",time) := floor(t_start/time)
-		     ][, list(missing=(sum(is.na(SDNN)) / .N * 100)), by=eval(paste0("t_",time))]
+		dt[, vars, with = FALSE
+		   ][, paste0("t_", tvar) := floor(t_start/time)
+		     ][, list(missing=(sum(is.na(SDNN)) / .N * 100)), by=eval(paste0("t_", tvar))]
 
 	# Keys for merging
-	setkeyv(x, paste0("t_", time))
-	setkeyv(y, paste0("t_", time))
-	z <- x[y]
+	setkeyv(x, paste0("t_", tvar))
+	setkeyv(y, paste0("t_", tvar))
+	z <- na.omit(x[y])
 
 	# Add patid
 	z <- cbind(patid = name, z)
