d209c60160158ddf3b253f03d6450ff2ca80ce6e
Anish S. Shah <asshah4@users.noreply.github.com>
2024-05-05 13:02:12 -0500

    init AF-CTRS function #4

---
+++ R/af-ctrs.R
@@ -0,0 +1,14 @@
+# This page is for the AF Composite Treatment Response Score
+
+#' Atrial Fibrillation Composite Treatment Response Score
+#' @export
+af_ctrs <- function(date,
+										ecg,
+										rhythm,
+										afeqt,
+										...) {
+  # This is a placeholder function
+  # It will be filled in with the actual implementation
+  # of the AF Composite Treatment Response Score
+  stop("This function is not yet implemented")
+}
